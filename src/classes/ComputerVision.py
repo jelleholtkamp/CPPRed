f2092f7d8692270265f1cd5cea3e0ccef8f626ff
Jelle Holtkamp <jholtkamp@unica.nl>
2023-06-27 20:53:58 +0200

    switch to pyboy screenshot function

--- src/classes/ComputerVision.py
+++ src/classes/ComputerVision.py
@@ -21,128 +21,54 @@ class MatchResult:
         self.height = endY - startY  
 
 class MatchTemplate: 
-    def OnScreen(templatePath, threshold, masks = None):
-        logging.info("Matching template " + templatePath + " with threshold " + str(threshold) + " on screen")
-        debugPath = "Debug\MatchTemplate\OnScreen"
+    def Match(session, template, threshold, masks = None, scale = 6):
+
+        templatePath = "TemplateMatching\\" + template + ".png"
+        debugPath = "Debug\MatchTemplate\\" + template + "\\"
         result = None
-        logging.info("Taking screenshot of entire screen")
-        screenshot = pyautogui.screenshot()
-        screenshotDebugPath = debugPath + "\screenshot.png"
-        screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-        screenshot_gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+
+        screenshotOriginalDebugPath = debugPath + template +  "Original.png"
+        screenshotMaskedDebugPath = debugPath + template + "Masked.png"
+        screenshotMatchedDebugPath = debugPath + template + "Matched.png"
+
+        screenshot = session.screen_image()
+        screenshot.save(screenshotOriginalDebugPath)
+        screenshot_color = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+        screenshot_color = cv2.resize(screenshot_color, (0,0), fx=scale, fy=scale)
+        screenshot_gray = cv2.cvtColor(screenshot_color, cv2.COLOR_BGR2GRAY)
         if masks != None:
             logging.info("Masking screenshot")
             for mask in masks:
-                screenshotMaskedDebugPath = debugPath + "\screenshotMasked.png"
-                cv2.rectangle(screenshot, (mask["startX"], mask["startY"]), (mask["endX"], mask["endY"]), (255, 255, 255), -1)
+                cv2.rectangle(screenshot_color, (mask["startX"], mask["startY"]), (mask["endX"], mask["endY"]), (255, 255, 255), -1)
                 logging.info("Saving masked screenshot to " + screenshotMaskedDebugPath)
-                cv2.imwrite(screenshotMaskedDebugPath, screenshot)
-            
-        template = cv2.imread(templatePath, 0)  
+                cv2.imwrite(screenshotMaskedDebugPath, screenshot_color)
+                
+        templateFile = cv2.imread(templatePath, 0)
 
-        w, h = template.shape[::-1]
+        w, h = templateFile.shape[::-1]
 
         logging.info("Matching template " + templatePath + " with threshold " + str(threshold))
-        matchResult = cv2.matchTemplate(screenshot_gray, template, cv2.TM_CCOEFF_NORMED)
+        matchResult = cv2.matchTemplate(screenshot_gray, templateFile, cv2.TM_CCOEFF_NORMED)
         location = np.where(matchResult >= threshold)
         for pt in zip(*location[::-1]):
-            cv2.rectangle(screenshot, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2)
+            cv2.rectangle(screenshot_color, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2)
             if pt != None:
                 logging.info("Template found")
                 (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(matchResult)
                 (startX, startY) = maxLoc
-                endX = startX + template.shape[1]
-                endY = startY + template.shape[0]
+                endX = startX + templateFile.shape[1]
+                endY = startY + templateFile.shape[0]
                 result = MatchResult(startX,startY,endX,endY)
-                logging.info("Saving debug screenshot to " + screenshotDebugPath)
-                cv2.imwrite(screenshotDebugPath, screenshot)
-                return result
+                logging.info("Saving matched screenshot to " + screenshotMatchedDebugPath)
+                cv2.imwrite(screenshotMatchedDebugPath, screenshot_color)
+                resultObject = {
+                    "screenshot": screenshot_color,
+                    "region": result
+                }
+                return resultObject
 
-    def InGameWindow(templatePath, threshold, masks = None):
-        debugPath = "Debug\MatchTemplate\InGameWindow"
-        result = None
-
-        screenshotDebugPath = debugPath + "\screenshot.png"
-        gameWindow = FindStuff.GameWindow()
-
-        if gameWindow != None:
-            screenshot = pyautogui.screenshot(region=(gameWindow["region"].startX,gameWindow["region"].startY,gameWindow["region"].width,gameWindow["region"].height))
-            print(screenshot)
-            screenshot_color = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-            screenshot_gray = cv2.cvtColor(screenshot_color, cv2.COLOR_BGR2GRAY)
-            if masks != None:
-                logging.info("Masking screenshot")
-                for mask in masks:
-                    screenshotMaskedDebugPath = debugPath + "\screenshotMasked.png"
-                    cv2.rectangle(screenshot_color, (mask["startX"], mask["startY"]), (mask["endX"], mask["endY"]), (255, 255, 255), -1)
-                    logging.info("Saving masked screenshot to " + screenshotMaskedDebugPath)
-                    cv2.imwrite(screenshotMaskedDebugPath, screenshot_color)
-                
-            template = cv2.imread(templatePath, 0)  
-
-            w, h = template.shape[::-1]
-
-            logging.info("Matching template " + templatePath + " with threshold " + str(threshold))
-            matchResult = cv2.matchTemplate(screenshot_gray, template, cv2.TM_CCOEFF_NORMED)
-            location = np.where(matchResult >= threshold)
-            for pt in zip(*location[::-1]):
-                cv2.rectangle(screenshot_color, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2)
-                if pt != None:
-                    logging.info("Template found")
-                    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(matchResult)
-                    (startX, startY) = maxLoc
-                    endX = startX + template.shape[1]
-                    endY = startY + template.shape[0]
-                    result = MatchResult(startX,startY,endX,endY)
-                    logging.info("Saving debug screenshot to " + screenshotDebugPath)
-                    cv2.imwrite(screenshotDebugPath, screenshot_color)
-                    resultObject = {
-                        "relativeRegion": result,
-                        "absoluteRegion": gameWindow["region"]
-                    }
-                    return resultObject
-
-class FindStuff:
-    def WindowBar():
-        logging.info("Finding window bar")
-        WindowBarLocation = MatchTemplate.OnScreen("TemplateMatching\WindowBar.png",0.8)
-        if WindowBarLocation != None:
-            logging.info("Window bar found")
-            global WindowBar
-            WindowBar = WindowBarLocation
-            return WindowBar
-        else:
-            logging.info("Window bar not found")
-            return "NotFound"
-        
-    def GameWindow():
-        logging.info("Finding game window")
-        windowBar = FindStuff.WindowBar()
-
-        if windowBar != None and windowBar != "NotFound":
-            logging.info("Window bar found")
-            gameWindow = windowBar
-            gameWindow.startX  = windowBar.startX
-            gameWindow.endX = windowBar.endX
-            gameWindow.startY = windowBar.startY + windowBar.height
-            gameWindow.endY = windowBar.endY + 864
-
-            result = MatchResult(gameWindow.startX,gameWindow.startY,gameWindow.endX,gameWindow.endY)
-            screenshot = pyautogui.screenshot(region=(gameWindow.startX,gameWindow.startY,result.width,result.height))
-
-            gameWindow.width = result.width
-            gameWindow.height = result.height
-
-            screenshot.save("Debug\GameWindow\screenshot.png")
-            result = {
-                "screenshot": screenshot,
-                "region": gameWindow
-            }
-            return result
-        else:
-            logging.info("Window bar not found")
-        
-    def ConvoBox():
+class FindStuff:       
+    def ConvoBox(session):
         logging.info("Finding convo box")
         masks = [
             {
@@ -152,13 +78,13 @@ class FindStuff:
                 "endY": 816
             }
         ]
-        ConvoBox = MatchTemplate.InGameWindow("TemplateMatching\\Convo.png", 0.8, masks)
+        ConvoBox = MatchTemplate.Match(session,"ConvoBox", 0.8, masks)
 
         if ConvoBox != None:
-            logging.info("Convo bar found")
-            return ConvoBox    
+            logging.info("Convo box found")
+            return ConvoBox
         else:
-            logging.info("Convo bar not found")
+            logging.info("Convo box not found")
             return "NotFound"
         
     # TODO: Recheck these. If not working, check if they match the ConvoBox function
@@ -202,16 +128,15 @@ class FindStuff:
     #         return "NotFound"
         
 class OCR:
-    def ReadConvoBox():
+    def ReadConvoBox(session):
         pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-        screenshot = FindStuff.ConvoBox()
+        screenshot = FindStuff.ConvoBox(session)
         if screenshot != "NotFound":
-            print(screenshot["absoluteRegion"].startX)
-            print(screenshot["absoluteRegion"].startY)
-            print(screenshot["absoluteRegion"].width)
-            print(screenshot["absoluteRegion"].height)
-            print("/////////////////////////////////")
-            print(screenshot["relativeRegion"].startX)
-            print(screenshot["relativeRegion"].startY)
-            print(screenshot["relativeRegion"].width)
-            print(screenshot["relativeRegion"].height)
+
+            print(screenshot["region"].startX)
+            print(screenshot["region"].startY)
+            print(screenshot["region"].width)
+            print(screenshot["region"].height)
+            # print("/////////////////////////////////")
+            # print(screenshot["screenshot"])
+      
